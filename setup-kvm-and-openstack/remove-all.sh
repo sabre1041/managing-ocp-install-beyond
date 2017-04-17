9aad7550a344e966bf1b81c3f51e001db2abbc02
Vinny Valdez <vvaldez@redhat.com>
2017-04-17 13:13:08 -0500

    Update statement about removing local images

--- setup-kvm-and-openstack/remove-all.sh
+++ setup-kvm-and-openstack/remove-all.sh
@@ -7,5 +7,6 @@ source remove-repo-vm.sh
 source remove-tower-vm.sh
 source remove-kvm-host-config.sh
 
-echo "INFO: Leave images to speed up deploy scripts. Remove images only if using build scripts."
+echo "INFO: Leave local images to speed up deploy scripts."
+echo "INFO: Remove local images to retrieve updated images or if using build scripts."
 rm -iv /tmp/${LAB_NAME}-*.qcow2
