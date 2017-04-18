a5d3485ea1755bb05c1703632cf1e7f48cace4db
Vinny Valdez <vvaldez@redhat.com>
2017-04-18 12:48:37 -0500

    Move prompt for removing image in tmp to remove function

--- setup-kvm-and-openstack/remove-all.sh
+++ setup-kvm-and-openstack/remove-all.sh
@@ -6,7 +6,3 @@ source remove-rhosp-vm.sh
 source remove-repo-vm.sh
 source remove-tower-vm.sh
 source remove-kvm-host-config.sh
-
-echo "INFO: Leave local images to speed up deploy scripts."
-echo "INFO: Remove local images to retrieve updated images or if using build scripts."
-rm -iv /tmp/${LAB_NAME}-*.qcow2
